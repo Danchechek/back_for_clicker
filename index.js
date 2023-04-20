8d422e2ada82f2a3c242cc9c70286bb3e69dc6e6
Danchechek <89932916+Danchechek@users.noreply.github.com>
2023-04-21 01:41:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
