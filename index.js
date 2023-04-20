4b91a58cd4042df22f32229d63e6c268ea6508d0
Danchechek <89932916+Danchechek@users.noreply.github.com>
2023-04-21 01:01:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 // список уникальных ключей
 const keys = [];
